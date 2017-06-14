027fd7f0e22ad485e194a55e6608fac14afc3dd1
derikwhittaker@quickenloans.com <derikwhittaker@quickenloans.com>
2017-06-14 07:31:36 -0400

    added nuget restore support to build file(s)

--- build/buildRockEncryptionNuget.bat
+++ build/buildRockEncryptionNuget.bat
@@ -1,2 +1,6 @@
-msbuild /p:Configuration=Release ..\Rock.Encryption\Rock.Encryption.csproj
+
+nuget restore -SolutionDirectory ../  ../Rock.Encryption/Rock.Encryption.csproj
+
+msbuild /p:Configuration=Release /t:Clean;Rebuild ..\Rock.Encryption\Rock.Encryption.csproj
+
 nuget pack ..\Rock.Encryption\Rock.Encryption.csproj -Properties Configuration=Release
--- build/buildRockEncryptionXSerializerNuget.bat
+++ build/buildRockEncryptionXSerializerNuget.bat
@@ -1,2 +1,6 @@
-msbuild /p:Configuration=Release ..\Rock.Encryption.XSerializer\Rock.Encryption.XSerializer.csproj
+
+nuget restore -SolutionDirectory ../  ../Rock.Encryption.XSerializer/Rock.Encryption.XSerializer.csproj
+
+msbuild /p:Configuration=Release /t:Clean;Rebuild ..\Rock.Encryption.XSerializer\Rock.Encryption.XSerializer.csproj
+
 nuget pack ..\Rock.Encryption.XSerializer\Rock.Encryption.XSerializer.csproj -Properties Configuration=Release
